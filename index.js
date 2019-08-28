254810e68e9adc15a42fa941703493dc92beb9eb
Joseph Simpson <joseph.simpson@thehomeslicegroup.com>
2019-08-28 16:30:02 -0600

    Move models into separate file

--- index.js
+++ index.js
@@ -5,17 +5,10 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = require('./models/blog')
 
 const mongoUrl = config.MONGODB_URI
+
 mongoose.connect(mongoUrl, {useNewUrlParser: true})
 
 app.use(cors())
